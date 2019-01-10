9cf2bb3d80f00e319c5169ddd508780f4a6a208b
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2019-01-09 20:58:58 -0500

    finalized

--- molecules.py
+++ molecules.py
@@ -4,40 +4,48 @@ Molecules Problem
 """
 # from itertools import permutations
 import itertools
+import os
 __author__ = "Jake J, Taylor G, Morgan"
 
-inpt = ['CDBADCBBEFEF',
-        'DACCBADAFEAB',
-        'EFBDCAADBDCD',
-        'ABCDABCDABCD',
-        'DACCBADAFEAB',
-        'EFBDCAADBDCD',
-        'ABCDABCDABCD',
-        'CDBADCBBEFEF',
-        'ABABABABABAB',
-        'CDCDCDCDCDCD',
-        'EEEEEEEEEEEE',
-        'FFFFFFFFFFFF',
-        'ABAAAAAAAABA',
-        'CBCCCCCCCCBC',
-        'DBDDDDDDDDBD',
-        'EBEEEEEEEEBE',
-        'ABBBBBBBBBBA',
-        'ACCCCCCCCCCA',
-        'ADDDDDDDDDDA',
-        'AEEEEEEEEEEA',
-        'BBBABBBABBBB',
-        'CCACCCACCCCC',
-        'DDDDADDADDDD',
-        'EEAEEAEEEEEE',
-        'Q']
+default_input_vals = ['CDBADCBBEFEF',
+                      'DACCBADAFEAB',
+                      'EFBDCAADBDCD',
+                      'ABCDABCDABCD',
+                      'DACCBADAFEAB',
+                      'EFBDCAADBDCD',
+                      'ABCDABCDABCD',
+                      'CDBADCBBEFEF',
+                      'ABABABABABAB',
+                      'CDCDCDCDCDCD',
+                      'EEEEEEEEEEEE',
+                      'FFFFFFFFFFFF',
+                      'ABAAAAAAAABA',
+                      'CBCCCCCCCCBC',
+                      'DBDDDDDDDDBD',
+                      'EBEEEEEEEEBE',
+                      'ABBBBBBBBBBA',
+                      'ACCCCCCCCCCA',
+                      'ADDDDDDDDDDA',
+                      'AEEEEEEEEEEA',
+                      'BBBABBBABBBB',
+                      'CCACCCACCCCC',
+                      'DDDDADDADDDD',
+                      'EEAEEAEEEEEE',
+                      'Q']
+
+
+def read_file(file):
+    print(file)
+    with open(file) as f:
+        lines = [line for line in f.readlines()
+                 if 'Q' not in line]
+    return(lines)
 
 
 def parse_input(lines):
     """take input strings and compile them into a
     list with dataset groups of 4"""
-    # reading and stripping input from .txt file right now for testing,
-    # will convert to sysargs for final
+
     # group lines in lists of len 4
     return [lines[i:i+4]
             for i in range(0, len(lines[:-1]), 4)]
@@ -55,16 +63,17 @@ def best_fit(w, h, across1, across2, down1, down2):
                 if across2[a2] != down1[d1 + h - 1]:
                     continue
                 for d2 in range(1, 12 - h):
-                    if down2[d2] != across1[a1+w - 1]:
+                    if down2[d2] != across1[a1 + w - 1]:
                         continue
-                    if down2[d2+h - 1] != across2[a2+w - 1]:
+                    if down2[d2+h - 1] != across2[a2 + w - 1]:
                         continue
+
                     return True
 
     return False
 
 
-def find_possible_rectangles(string_length):
+def possible_rectangles(string_length):
     height = string_length - 1
     pairs = [(w, h)for w in range(2, height) for h in range(w, height)]
     # sort from biggest area to smallest
@@ -72,15 +81,14 @@ def find_possible_rectangles(string_length):
     return pairs
 
 
-def find_largest_area(dataset):
-    pairs = find_possible_rectangles(12)
+def largest_area(dataset):
+    pairs = possible_rectangles(12)
     perms = itertools.permutations(dataset)
     fits = []
     for across1, across2, down1, down2 in perms:
         for w, h in pairs:
-            fit = best_fit(w, h, across1, across2, down1, down2)
-            if fit:
-                fits.append((w-2)*(h-2))
+            if best_fit(w, h, across1, across2, down1, down2):
+                fits.append((w-2) * (h-2))
 
     largest_area = sorted(fits, reverse=True)[0] if fits else 0
     return largest_area
@@ -88,9 +96,16 @@ def find_largest_area(dataset):
 
 def main(inpt):
     vals = parse_input(inpt)
+
     for grp in vals:
-        print(find_largest_area(grp))
+        print(largest_area(grp))
 
 
 if __name__ == "__main__":
-    main(inpt)
+    print(os.listdir('.'))
+    if 'sample_input.txt' in os.listdir('.'):
+        input_vals = read_file('sample_input.txt')
+    else:
+        input_vals = default_input_vals
+    print(input_vals)
+    main(input_vals)
